1d421211ef224110d108c4d44ef25fa9bd9e63aa
Gaje-Cornerstone <grobarge@cornerstoneav.com>
2021-08-30 10:49:14 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header class="main-header">
+        <div class="header-container">
+          <h1 class="mh-logo">
+            <img src="http://flexbox.ninja/assets/images/logo.svg" width="170" height="95" alt="Flexbox.ninja"></img>
+          </h1>
+          <nav class="main-nav">
+            <ul class="main-nav-list">
+              <li>
+                <a href="#">Home</a>
+              </li>
+              <li>
+                <a href="#">About us</a>
+              </li>
+              <li>
+                <a href="#">Our Work</a>
+              </li>
+              <li>
+                <a href="#">Contact</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
       </header>
+      <div className="section">About Us</div>
+      <div className="section">Our Work</div>
+      <div className="section">Contact Us</div>
     </div>
   );
 }
 
 export default App;
+
